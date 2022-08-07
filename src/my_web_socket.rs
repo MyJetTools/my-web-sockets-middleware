8e076107fc21c6ace64d8311ed7167d66baf49b8
amigin <amigin@gmail.com>
2022-08-07 15:21:55 +0300

    Made Disconnect

--- src/my_web_socket.rs
+++ src/my_web_socket.rs
@@ -10,7 +10,7 @@ use my_http_server::{UrlEncodedData, UrlEncodedDataSource};
 use tokio::sync::Mutex;
 
 pub struct MyWebSocket {
-    pub write_stream: Mutex<SplitSink<WebSocketStream<Upgraded>, Message>>,
+    pub write_stream: Mutex<Option<SplitSink<WebSocketStream<Upgraded>, Message>>>,
     pub addr: SocketAddr,
     pub id: i64,
     query_string: Option<String>,
@@ -24,16 +24,29 @@ impl MyWebSocket {
         query_string: Option<String>,
     ) -> Self {
         Self {
-            write_stream: Mutex::new(write_stream),
+            write_stream: Mutex::new(Some(write_stream)),
             addr,
             id,
             query_string,
         }
     }
 
-    pub async fn send_message(&self, msg: Message) -> Result<(), Error> {
+    async fn send_message_and_if_conntext(&self, msg: Message) -> Result<(), Error> {
         let mut write_access = self.write_stream.lock().await;
-        write_access.send(msg).await
+        if let Some(stream) = &mut *write_access {
+            return stream.send(msg).await;
+        }
+
+        Ok(())
+    }
+
+    pub async fn send_message(&self, msg: Message) {
+        let result = self.send_message_and_if_conntext(msg).await;
+
+        if let Err(err) = result {
+            println!("Error sending message to websocket {}: {:?}", self.id, err);
+            self.disconnect().await;
+        }
     }
 
     pub fn get_query_string<'s>(&'s self) -> Option<UrlEncodedData<'s>> {
@@ -44,6 +57,17 @@ impl MyWebSocket {
             Err(_) => {
                 println!("Can not parse query string: {}", str);
                 return None;
+            }
+        }
+    }
+
+    pub async fn disconnect(&self) {
+        let mut write_access = self.write_stream.lock().await;
+        if let Some(mut item) = write_access.take() {
+            let result = item.close().await;
+
+            if let Err(err) = result {
+                println!("Can not close websocket {}. Reason: {:?}", self.id, err);
             }
         }
     }
